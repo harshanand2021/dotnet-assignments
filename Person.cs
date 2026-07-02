de31371673561b742e2702d1693d95ab6f46faa6
Harsh Anand <harsh25061999@gmail.com>
2026-07-02 20:03:11 +0530

    Assignment3_1 Dotnet

---
+++ Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Assignment3_1
+{
+	public class Person
+	{
+		public string FirstName { get; set;  }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+
+		//Default Constructor with default values
+		public Person(){
+			FirstName = "\0";
+			LastName = "\0";
+			Age = 0;
+		}
+
+
+		//Initialized Constructor with passed values
+		public Person(string first_name, string last_name, int age)
+		{
+			FirstName = first_name;
+			LastName = last_name;
+			Age = age;
+		}
+
+		public string GetFullName(Person p)
+		{
+			return p.FirstName + " " + p.LastName;
+		}
+
+		public bool IsAdult(Person p)
+		{
+			if (p.Age >= 18)
+				return true;
+			else
+				return false;
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Assignment3_1;
+
+class Program
+{
+    static void DisplayDetails(Person p)
+    {
+        Console.WriteLine("First Name = " + p.FirstName);
+        Console.WriteLine("Last Name = " + p.LastName);
+        Console.WriteLine("Age = " + p.Age);
+    }
+
+    static void Main(string[] args)
+    {
+        Person person;
+
+        Console.Write("Enter first name: ");
+        string FirstName = Console.ReadLine();
+
+        Console.Write("Enter last name: ");
+        string LastName = Console.ReadLine();
+
+        Console.Write("Enter age: ");
+        int Age = Console.Read();
+
+        person = new Person(FirstName, LastName, Age);
+
+        string FullName = person.GetFullName(person);
+        Console.WriteLine("Full Name = " + FullName);
+
+        Console.WriteLine("Details of person - ");
+
+        DisplayDetails(person);
+
+    }
+}
+
